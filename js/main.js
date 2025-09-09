3c322992d7dcf0997607621ad70021a21d20f90a
Pablo Gallo <gallopabloj@gmail.com>
2025-09-09 16:56:10 -0300

    cambio de estilos

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btnNav = document.getElementById('nav-switch')
+    const nav = document.getElementById('nav-section')
+
+    function toggleMenu() {
+        nav.hidden = !(nav.hidden)
+    }
+    
+    btnNav.addEventListener('click', toggleMenu)
+
+    document.addEventListener('click', (e) => {
+        if (!nav.contains(e.target) && !btnNav.contains(e.target) && nav.hidden == false) {
+            toggleMenu()
+        }
+    })
+})
